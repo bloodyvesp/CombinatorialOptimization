574b6337ee5833361dd6041f3447831c0d10426f
htellez <htellez@nearbpo.com>
2013-11-20 03:10:04 -0600

    Completing simplex algorithm.

--- Simplex/simplex.m
+++ Simplex/simplex.m
@@ -14,8 +14,44 @@ function algo = simplex(M, c, b)
 	lambda = c_B * B_inv
 
 	discriminant = c_N - lambda * N;
-	if(all(discriminant))
-		disp("Optimum found");
+	if(all(discriminant >= 0))
 		algo = [x_B0, x_N0];
+		disp("Optimum found.");
+		disp("Value: ");
+		disp(c * algo');
+		return;
 	endif
+	
+	pivote_found = false
+	for j = 1:columns-rows
+		discriminant = c_N(j) - lambda * N(:, j)
+		if(all(discriminant < 0))
+			pivote_found = true
+			break;
+		endif
+	endfor
+	
+	if(!pivote_found)
+		error("j pivote not found!");
+	endif
+	
+	D_j = B_inf * N(:,j);
+	
+	if(all(D_j <= 0))
+		error("Not bounded.");
+	endif
+	
+	discriminant = 0;
+	for i = 1:rows
+		if(D_j(i) <= 0)
+			discriminant(i) = Inf;
+		endif
+		
+		discriminant(i) = x_B0(i)/D_j(i);
+	endfor
+	
+	[minimum, i_s] = min(discriminant);
+	M_s = M;
+	M_s(: [i_s, rows+j]) = M_s(: [rows+j, i_s])
+	algo = simplex(M_s, c, b);
 endfunction
